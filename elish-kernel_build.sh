48a4dfe40c79c4b92351c62bad46401909d20e27
Fauzan Amir Al Ghiffary <alghiffaryfa19@gmail.com>
2025-06-01 22:16:10 +0700

    update mkbootimg script

--- elish-kernel_build.sh
+++ elish-kernel_build.sh
@@ -6,21 +6,18 @@ cp config-kupferlinux-qcom-sm8250.aarch64 .config
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
 _kernel_version="$(make kernelrelease -s)"
 
-mkdir ../linux-xiaomi-elish/boot
-cp $1/linux/arch/arm64/boot/Image.gz $1/linux-xiaomi-elish/boot/vmlinuz-$_kernel_version
-cp $1/linux/arch/arm64/boot/dts/qcom/sm8250-xiaomi-elish-csot.dtb $1/linux-xiaomi-elish/boot/dtb-$_kernel_version
 
 sed -i "s/Version:.*/Version: ${_kernel_version}/" $1/linux-xiaomi-elish/DEBIAN/control
 
 chmod +x $1/mkbootimg
 
-cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8250-xiaomi-elish-csot.dtb > $1/linux/Image_w_dtb
-gzip Image_w_dtb
-$1/mkbootimg --header_version 0 --base 0x0 --cmdline "clk_ignore_unused pd_ignore_unused root=PARTLABEL=linux" --kernel $1/linux/Image_w_dtb.gz -o $1/boot_csot.img
+cat $1/linux/arch/arm64/boot/Image.gz $1/linux/arch/arm64/boot/dts/qcom/sm8250-xiaomi-elish-csot.dtb > $1/linux/Image.gz-dtb_csot
+mv $1/linux/Image.gz-dtb_csot $1/linux/zImage_csot
+$1/mkbootimg --kernel zImage_csot --cmdline "root=PARTLABEL=linux" --base 0x00000000 --kernel_offset 0x00008000 --tags_offset 0x00000100 --pagesize 4096 --id -o $1/boot_csot.img
 
-cat $1/linux/arch/arm64/boot/Image $1/linux/arch/arm64/boot/dts/qcom/sm8250-xiaomi-elish-boe.dtb > $1/linux/Image_w_dtb
-gzip Image_w_dtb
-$1/mkbootimg --header_version 0 --base 0x0 --cmdline "clk_ignore_unused pd_ignore_unused root=PARTLABEL=linux" --kernel $1/linux/Image_w_dtb.gz -o $1/boot_boe.img
+cat $1/linux/arch/arm64/boot/Image.gz $1/linux/arch/arm64/boot/dts/qcom/sm8250-xiaomi-elish-boe.dtb > $1/linux/Image.gz-dtb_boe
+mv $1/linux/Image.gz-dtb_boe $1/linux/zImage_boe
+$1/mkbootimg --kernel zImage_boe --cmdline "root=PARTLABEL=linux" --base 0x00000000 --kernel_offset 0x00008000 --tags_offset 0x00000100 --pagesize 4096 --id -o $1/boot_boe.img
 
 rm $1/linux-xiaomi-elish/usr/dummy
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$1/linux-xiaomi-elish/usr modules_install
