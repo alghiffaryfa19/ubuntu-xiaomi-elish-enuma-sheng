e8d9c548fc3819bb48f5d988b343dcb2aa8f6c35
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2025-06-14 23:32:13 +0700

    Update sheng-kernel_build.sh

--- sheng-kernel_build.sh
+++ sheng-kernel_build.sh
@@ -13,7 +13,7 @@ chmod +x $1/mkbootimg
 
 cat $1/linux/arch/arm64/boot/Image.gz $1/linux/arch/arm64/boot/dts/qcom/sm8550-xiaomi-sheng.dtb > $1/linux/Image.gz-dtb_sheng
 mv $1/linux/Image.gz-dtb_sheng $1/linux/zImage_sheng
-$1/mkbootimg --kernel zImage_sheng --cmdline "root=PARTLABEL=linux" --base 0x00000000 --kernel_offset 0x00008000 --tags_offset 0x01e00000 --pagesize 4096 --id -o $1/boot_sheng.img
+$1/mkbootimg --kernel zImage_sheng --cmdline "root=PARTLABEL=userdata" --base 0x00000000 --kernel_offset 0x00008000 --tags_offset 0x01e00000 --pagesize 4096 --id -o $1/boot_sheng.img
 
 #rm $1/linux-xiaomi-sheng/usr/dummy
 make -j$(nproc) ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD_PATH=$1/linux-xiaomi-sheng/usr modules_install
